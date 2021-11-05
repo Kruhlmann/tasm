2aa483904a7ce42e28860313058f3272d5671033
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-11-05 11:29:34 +0100

    Re-introduce missing push int file

---
+++ lib/tasm/operations/atomic/push_int.rb
@@ -0,0 +1,26 @@
+require "tasm/operations/arg_operation"
+
+class PushIntOperation < ArgOperation
+  def execute(stack, instruction_ptr)
+    stack.append(@argument)
+    return instruction_ptr += 1
+  end
+
+  def asm_instruction(operation_index, _)
+    <<~EOS
+      push #{@argument}
+    EOS
+  end
+
+  def nodejs_instruction(operation_index)
+    <<~EOS
+      stack.push(#{@argument});
+    EOS
+  end
+
+  def lua_instruction(operation_index)
+    <<~EOS
+      table.insert(stack, #{@argument});
+    EOS
+  end
+end
